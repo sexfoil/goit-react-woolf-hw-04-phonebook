2730c69b1514d6a00f54b579daf2e5cf6b795a5b
Slava Poliakov <78slavapolyakov@gmail.com>
2024-02-24 11:39:55 +0200

    Refactored with hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,107 +1,86 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import ContactForm from './ContactForm/ContactForm';
 import Filter from './Filter/Filter';
 import ContactList from './ContactList/ContactList';
 
-class App extends Component {
-  #LS_CONTACTS_KEY = 'contacts';
+const App = () => {
+  const CONTACTS_LS_KEY = 'contacts';
 
-  state = {
-    contacts: [],
-    filter: '',
-  };
+  const [isMounted, setIsMounted] = useState(false);
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
 
-  componentDidMount() {
-    const savedContacts = localStorage.getItem(this.#LS_CONTACTS_KEY);
+  useEffect(() => {
+    const savedContacts = localStorage.getItem(CONTACTS_LS_KEY);
     if (savedContacts) {
-      this.setState({ contacts: JSON.parse(savedContacts) });
+      setContacts(JSON.parse(savedContacts));
     }
-  }
+    setIsMounted(true);
+  }, []);
 
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.contacts.length !== prevState.contacts.length) {
-      localStorage.setItem(
-        this.#LS_CONTACTS_KEY,
-        JSON.stringify(this.state.contacts)
-      );
+  useEffect(() => {
+    if (isMounted) {
+      const contactsJsonString = JSON.stringify(contacts);
+      localStorage.setItem(CONTACTS_LS_KEY, contactsJsonString);
     }
-  }
+  }, [contacts, isMounted]);
 
-  addContact = contact => {
-    if (this.hasContact(contact.name)) {
+  const addContact = contact => {
+    if (hasContact(contact.name)) {
       alert(`${contact.name} is already in contacts.`);
       return false;
     }
 
-    this.setState(prev => {
-      const updatedContacts = [...prev.contacts, contact];
-      return {
-        contacts: updatedContacts,
-      };
-    });
+    setContacts([...contacts, contact]);
     return true;
   };
 
-  removeContact = contactId => {
-    this.setState(prev => {
-      const updatedContacts = prev.contacts.filter(
-        contact => contact.id !== contactId
-      );
-      return {
-        contacts: updatedContacts,
-      };
-    });
+  const removeContact = contactId => {
+    const updatedContacts = contacts.filter(
+      contact => contact.id !== contactId
+    );
+    setContacts(updatedContacts);
   };
 
-  hasContact = name => {
-    return this.state.contacts.find(
-      element => element.name.toLowerCase().trim() === name.toLowerCase().trim()
+  const hasContact = name => {
+    return contacts.find(
+      contact => contact.name.toLowerCase().trim() === name.toLowerCase().trim()
     );
   };
 
-  updateFilter = value => {
-    this.setState(() => {
-      return { filter: value };
-    });
-  };
-
-  getFilteredContacts = () => {
-    if (!this.state.filter.trim()) {
-      return this.state.contacts;
+  const getFilteredContacts = () => {
+    if (!filter.trim()) {
+      return contacts;
     }
 
-    return this.state.contacts.filter(contact =>
-      contact.name
-        .toLowerCase()
-        .includes(this.state.filter.toLowerCase().trim())
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase().trim())
     );
   };
 
-  render() {
-    return (
-      <div
-        style={{
-          height: '100vh',
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'start',
-          alignItems: 'start',
-          paddingLeft: '24px',
-          fontSize: 18,
-          color: '#010101',
-        }}
-      >
-        <h1>Phonebook</h1>
-        <ContactForm addContact={this.addContact} />
-        <h2>Contacts</h2>
-        <Filter filter={this.state.filter} updateFilter={this.updateFilter} />
-        <ContactList
-          contacts={this.getFilteredContacts()}
-          removeContact={this.removeContact}
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'start',
+        alignItems: 'start',
+        paddingLeft: '24px',
+        fontSize: 18,
+        color: '#010101',
+      }}
+    >
+      <h1>Phonebook</h1>
+      <ContactForm addContact={addContact} />
+      <h2>Contacts</h2>
+      <Filter filter={filter} updateFilter={setFilter} />
+      <ContactList
+        contacts={getFilteredContacts()}
+        removeContact={removeContact}
+      />
+    </div>
+  );
+};
 
 export default App;
